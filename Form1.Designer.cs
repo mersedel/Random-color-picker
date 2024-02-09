c01af59ce66a642e4beb43c4a13755faba895053
mersedel <xr6033851@gmail.com>
2024-02-09 18:17:45 +0200

    Added files

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Random_color_picker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nextColorButton = new System.Windows.Forms.Button();
+            this.copyCurColorButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nextColorButton
+            // 
+            this.nextColorButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nextColorButton.Location = new System.Drawing.Point(652, 373);
+            this.nextColorButton.Name = "nextColorButton";
+            this.nextColorButton.Size = new System.Drawing.Size(120, 35);
+            this.nextColorButton.TabIndex = 0;
+            this.nextColorButton.Text = "Next color";
+            this.nextColorButton.UseVisualStyleBackColor = true;
+            this.nextColorButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.nextColorButton_MouseClick);
+            // 
+            // copyCurColorButton
+            // 
+            this.copyCurColorButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.copyCurColorButton.Location = new System.Drawing.Point(652, 414);
+            this.copyCurColorButton.Name = "copyCurColorButton";
+            this.copyCurColorButton.Size = new System.Drawing.Size(120, 35);
+            this.copyCurColorButton.TabIndex = 1;
+            this.copyCurColorButton.Text = "Copy current color";
+            this.copyCurColorButton.UseVisualStyleBackColor = true;
+            this.copyCurColorButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.copyCurColorButton_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.copyCurColorButton);
+            this.Controls.Add(this.nextColorButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Color picker by mersedel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button nextColorButton;
+        public System.Windows.Forms.Button copyCurColorButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Random_color_picker
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nextColorButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            ChangeBackgroundColor();
+        }
+
+        private void copyCurColorButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            Color backgroundColor = this.BackColor;
+
+            // Copy RGB to clipboard
+            Clipboard.SetText($"{backgroundColor.R}:{backgroundColor.G}:{backgroundColor.B}");
+
+            MessageBox.Show("The background color has been copied to the clipboard.", "Successfully copied");
+        }
+
+        private void ChangeBackgroundColor()
+        {
+            // Random numbers for color generation
+            int red = random.Next(256);
+            int green = random.Next(256);
+            int blue = random.Next(256);
+
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+    }
+}
